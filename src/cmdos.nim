894496ce4f77ac9e11ee8bb852f0a0fc37b27b94
Hecdin <Hecdinnicola@outlook.com>
2024-06-28 07:38:22 -0400

    1.0.0 (release version): + Add a more consistent syntax
    * More coherent and easy to use syntax.
    * Short arguments allowed to be added.
    * Logic and error handling improved.

---
+++ src/cmdos.nim
@@ -0,0 +1,66 @@
+import os
+
+type
+  Arg* = object
+    short*: string
+    long*: string
+    default*: string
+
+  Cmdos* = object
+    args*: seq[Arg]
+    values*: seq[string]
+
+var skipFirst: bool
+
+#-- Fusionar dos secuencias de argumentos (args, inputs)
+proc mergeArgsInputs(self: Cmdos, A, B: seq[string]): seq[string] =
+  var values: seq[string]
+  var start = 0
+
+  if skipFirst == true:
+    start = 1
+
+  for arg in self.args:
+    var found = false
+    for j in countup(start, B.len - 1, 2):
+      if B[j] == arg.long or B[j] == arg.short:
+        values.add(B[j])
+        values.add(B[j + 1])
+        found = true
+        break
+    if not found:
+      values.add(arg.long)
+      values.add(arg.default)
+  return values
+
+#-- Procesar los argumentos de entrada
+proc processArgs*(self: Cmdos, ignoreFirst: bool = false): seq[string] =
+  var inputs, default, data: seq[string]
+  skipFirst = ignoreFirst
+
+  # Verificar si el número de inputs tiene pares clave-valor
+  add(inputs, commandLineParams())
+  if (inputs.len %% 2 == 0) == skipFirst:
+    echo "Error: Invalid argument."
+    return
+
+  # Crear una secuencia de argumentos predeterminados y valores
+  for i in 1..(self.args).len:
+    add(default, self.args[i - 1].long)
+    add(default, self.args[i - 1].short)
+    add(default, self.args[i - 1].default)
+
+  # Fusionar los argumentos predeterminados con los argumentos de entrada
+  add(data, mergeArgsInputs(self, default, inputs))
+  return data
+
+#-- Extraer el valor de los argumentos
+proc extractPairs*(data: seq[string]): (seq[string], seq[string]) =
+  var args, values: seq[string]
+
+  for i in 1..(data).len:
+    if i %% 2 == 0:
+      add(values, data[i - 1])
+    else:
+      add(args, data[i - 1])
+  return (args, values)
