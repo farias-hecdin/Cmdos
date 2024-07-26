b529b432700054ab747eb5702eb85dc0a2ac72ba
Hecdin <Hecdinnicola@outlook.com>
2024-07-26 15:38:57 -0400

    2.0.0-dev.5: Refactor the code

--- src/cmdos.nim
+++ src/cmdos.nim
@@ -1,9 +1,9 @@
-import std/[os, strutils, sequtils, terminal], pkg/[tinyre]
+import std/[os, strutils, terminal], pkg/[tinyre]
 
 type
   CmdosArg* = object
     names*, inputs*: seq[string]
-    desc*: string
+    desc*, placeholder*: string
 
   CmdosCmd* = object
     names*: seq[string]
@@ -13,7 +13,6 @@ type
   Cmdos* = object
     name*, version*: string
     cmds*: seq[CmdosCmd]
-    help*: tuple[margin = 2, spacing = 3, styled: bool]
 
 type
   CmdosType* = static[array[1, Cmdos]]
@@ -59,76 +58,5 @@ proc processArgs*(cmd: CmdosCmd, ignoreFirst: bool, inputArgs: seq[string] = def
       argValues.add(argName)
       argValues.add(arg.inputs)
     result.add((data: argValues))
-)
-
-# HELP SCREEN -----------------------------------------------------------------
-
-#-- ANSI Color code
-# Thanks to: https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
-const
-  bold = "\e[1;97m"
-  underline = bold & "\e[4;37m"
-  reset = "\e[0m"
-
-#-- Obtener la palabra mas larga de un arreglo y su longitud
-proc getLongestWord(words: seq[string]): (string, int) = (
-  var longestWord: string
-  var maxLength = 0
-
-  for word in words:
-    if word.len > maxLength:
-      maxLength = word.len
-      longestWord = word
-  result = (longestWord, maxLength)
-)
-
-#-- Create the Usage section
-proc formatUsage(data: CmdosType): seq[string] = (
-  for commandGroup in data:
-    for command in commandGroup.cmds:
-      result.add("[" & command.names.join("/") & "]")
-)
-
-#-- Create the Commands section
-proc formatCommands(data: CmdosType, margin: string): seq[string] = (
-  let cmdos = data[0]
-  let commands = cmdos.cmds
-
-  # Find the maximum length of command name
-  let maxCommandNameLength = commands.mapIt(it.names.join(", ").len).max
-  # Format command lines
-  for command in commands:
-    let commandName = command.names.join(", ")
-    let spacing = " ".repeat(maxCommandNameLength - commandName.len + cmdos.help.spacing)
-    result.add(margin & commandName & spacing & command.desc)
-)
-
-#-- Create the Options section
-proc formatOptions(data: CmdosType, margin: string): seq[string] = (
-  let cmdos = data[0]
-  let commands = cmdos.cmds
-
-  for command in commands:
-    if command.args.len > 0:
-      var (word, _) = getLongestWord(command.names)
-      result.add("\n$#$# options:$#" % [underline, word, reset])
-      let maxArgNameLength = command.args.mapIt(it.names.join(", ").len).max
-      for arg in command.args:
-        let argName = arg.names.join(", ")
-        let spacing = " ".repeat(maxArgNameLength - argName.len + cmdos.help.spacing)
-        result.add(margin & arg.names.join(", ") & " " & spacing & arg.desc)
-)
-
-#-- Process the help screen
-proc processHelp*(data: CmdosType): string = (
-  const cmdos = data[0]
-  const app: string = cmdos.name
-  const margin: string = (" ").repeat(cmdos.help.margin)
-
-  const usageSection = "$#Usage:$#\n$#$# $#\n" % [underline, reset, margin, app, formatUsage(data).join(" ")]
-  const commandsSection = "$#Commands:$#\n$#" % [underline, reset, formatCommands(data, margin).join("\n")]
-  const optionsSection = formatOptions(data, margin).join("\n")
-
-  result = [usageSection, commandsSection, optionsSection].join("\n") & "\n"
 )
 
---
+++ src/help.nim
@@ -0,0 +1,69 @@
+import std/[strutils, sequtils]
+import cmdos
+
+const margin: string = (" ").repeat(2)
+
+#-- ANSI Color code (thanks to: https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124)
+const
+  bold = "\e[1;97m"
+  underline = bold & "\e[4;37m"
+  reset = "\e[0m"
+
+#-- Obtener la palabra mas larga de un arreglo y su longitud
+proc getLongestWord(words: seq[string]): (string, int) = (
+  var longestWord: string
+  var maxLength = 0
+
+  for word in words:
+    if word.len > maxLength:
+      maxLength = word.len
+      longestWord = word
+  result = (longestWord, maxLength)
+)
+
+#-- Create the Usage section
+proc formatUsage(data: CmdosType): seq[string] = (
+  for group in data:
+    for cmd in group.cmds:
+      result.add("[" & cmd.names.join("/") & "]")
+)
+
+#-- Create the Commands section
+proc formatCommands(data: CmdosType): seq[string] = (
+  let cmds = data[0].cmds
+  let maxCmdLen = cmds.mapIt(it.names.join(", ").len).max
+
+  for cmd in cmds:
+    let cmdName = cmd.names.join(", ")
+    let spacing = (" ").repeat(maxCmdLen - cmdName.len + 3)
+    result.add(margin & cmdName & spacing & cmd.desc)
+)
+
+#-- Create the Options section
+proc formatOptions(data: CmdosType): seq[string] = (
+  let cmds = data[0].cmds
+  for cmd in cmds:
+    if cmd.args.len > 0:
+      let (longestName, _) = getLongestWord(cmd.names)
+      let placeholderLen: int = cmd.args.mapIt(it.placeholder.len).max
+      let maxArgLen: int = cmd.args.mapIt(it.names.join(", ").len).max + placeholderLen
+      result.add("\n$#$# options:$#" % [underline, longestName, reset])
+
+      for arg in cmd.args:
+        let argName = arg.names.join(", ") & " " & arg.placeholder
+        let spacing = (" ").repeat(maxArgLen - argName.len + 3)
+        result.add(margin & argName & " " & spacing & arg.desc)
+)
+
+#-- Process the help screen
+proc processHelp*(data: CmdosType): string = (
+  const cmdos = data[0]
+  const app: string = cmdos.name
+
+  const usageSection = "$1Usage:$2\n$4$3 $5\n" % [underline, reset, app, margin, formatUsage(data).join(" ")]
+  const commandsSection = "$1Commands:$2\n$3" % [underline, reset, formatCommands(data).join("\n")]
+  const optionsSection = formatOptions(data).join("\n")
+
+  result = [usageSection, commandsSection, optionsSection].join("\n") & "\n"
+)
+
