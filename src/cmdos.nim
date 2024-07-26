bcce933112d6bcec7913f1a8f650f1afdeb6ecda
Hecdin <Hecdinnicola@outlook.com>
2024-07-26 09:26:57 -0400

    feat: Add ANSI color and add comments

--- src/cmdos.nim
+++ src/cmdos.nim
@@ -3,7 +3,7 @@ import std/[os, strutils, sequtils, terminal], pkg/[tinyre]
 type
   CmdosArg* = object
     names*, inputs*: seq[string]
-    desc*, placeholder*: string
+    desc*: string
 
   CmdosCmd* = object
     names*: seq[string]
@@ -24,8 +24,8 @@ proc showError(location, message: string) = (
   quit(QuitFailure)
 )
 
-#-- Procesar los argumentos de entrada
-var defaultArgs: seq[string] = os.commandLineParams()
+#-- Process input arguments
+let defaultArgs: seq[string] = os.commandLineParams()
 
 proc processArgs*(cmd: CmdosCmd, ignoreFirst: bool, inputArgs: seq[string] = defaultArgs): seq[tuple[data: seq[string]]] = (
   let startIndex = if ignoreFirst: 1 else: 0
@@ -38,21 +38,23 @@ proc processArgs*(cmd: CmdosCmd, ignoreFirst: bool, inputArgs: seq[string] = def
 
     for i in startIndex..<inputArgs.len:
       if inputArgs[i] in arg.names:
-        argFound = true
-
         var j = (i + 1)
+        argFound = true
+        # If the argument receives more inputs
         if arg.inputs.len > 1:
           argValues.add(argName)
           while j < inputArgs.len and not tinyre.contains(inputArgs[j], reExclude):
             argValues.add(inputArgs[j])
             inc(j)
+        # If only one argument is provided
         else:
           argValues.add(argName)
           if j < inputArgs.len:
             argValues.add(inputArgs[j])
         break
+    # If no argument is provided, default values will be used
     if not argFound:
-      if arg.inputs.len < 1:
+      if arg.inputs.len <= 0:
         showError(cmd.names[0], "The number of inputs provided is invalid.")
       argValues.add(argName)
       argValues.add(arg.inputs)
@@ -60,6 +62,13 @@ proc processArgs*(cmd: CmdosCmd, ignoreFirst: bool, inputArgs: seq[string] = def
 )
 
 # HELP SCREEN -----------------------------------------------------------------
+
+#-- ANSI Color code
+# Thanks to: https://gist.github.com/JBlond/2fea43a3049b38287e5e9cefc87b2124
+const
+  bold = "\e[1;97m"
+  underline = bold & "\e[4;37m"
+  reset = "\e[0m"
 
 #-- Obtener la palabra mas larga de un arreglo y su longitud
 proc getLongestWord(words: seq[string]): (string, int) = (
@@ -70,31 +79,31 @@ proc getLongestWord(words: seq[string]): (string, int) = (
     if word.len > maxLength:
       maxLength = word.len
       longestWord = word
-  return (longestWord, maxLength)
+  result = (longestWord, maxLength)
 )
 
-#-- Crear la seccion Usage
+#-- Create the Usage section
 proc formatUsage(data: CmdosType): seq[string] = (
   for commandGroup in data:
     for command in commandGroup.cmds:
       result.add("[" & command.names.join("/") & "]")
 )
 
-#-- Crear la seccion Commands
+#-- Create the Commands section
 proc formatCommands(data: CmdosType, margin: string): seq[string] = (
   let cmdos = data[0]
   let commands = cmdos.cmds
 
-  # Busca la longitud máxima del nombre de comando
+  # Find the maximum length of command name
   let maxCommandNameLength = commands.mapIt(it.names.join(", ").len).max
-  # Formatea las líneas de comando
+  # Format command lines
   for command in commands:
     let commandName = command.names.join(", ")
     let spacing = " ".repeat(maxCommandNameLength - commandName.len + cmdos.help.spacing)
     result.add(margin & commandName & spacing & command.desc)
 )
 
-#-- Crear la seccion Options
+#-- Create the Options section
 proc formatOptions(data: CmdosType, margin: string): seq[string] = (
   let cmdos = data[0]
   let commands = cmdos.cmds
@@ -102,24 +111,24 @@ proc formatOptions(data: CmdosType, margin: string): seq[string] = (
   for command in commands:
     if command.args.len > 0:
       var (word, _) = getLongestWord(command.names)
-      result.add("\n" & word & " options:")
+      result.add("\n$#$# options:$#" % [underline, word, reset])
       let maxArgNameLength = command.args.mapIt(it.names.join(", ").len).max
       for arg in command.args:
         let argName = arg.names.join(", ")
         let spacing = " ".repeat(maxArgNameLength - argName.len + cmdos.help.spacing)
-        result.add(margin & arg.names.join(", ") & spacing & arg.desc)
+        result.add(margin & arg.names.join(", ") & " " & spacing & arg.desc)
 )
 
 #-- Process the help screen
 proc processHelp*(data: CmdosType): string = (
   const cmdos = data[0]
-  const appName: string = cmdos.name
+  const app: string = cmdos.name
   const margin: string = (" ").repeat(cmdos.help.margin)
 
-  const usageSection = "Usage:\n$1$2 $3\n" % [margin, appName, formatUsage(data).join(" ")]
-  const commandsSection = "Commands:\n$1" % [formatCommands(data, margin).join("\n")]
+  const usageSection = "$#Usage:$#\n$#$# $#\n" % [underline, reset, margin, app, formatUsage(data).join(" ")]
+  const commandsSection = "$#Commands:$#\n$#" % [underline, reset, formatCommands(data, margin).join("\n")]
   const optionsSection = formatOptions(data, margin).join("\n")
 
-  return [usageSection, commandsSection, optionsSection].join("\n") & "\n"
+  result = [usageSection, commandsSection, optionsSection].join("\n") & "\n"
 )
 
